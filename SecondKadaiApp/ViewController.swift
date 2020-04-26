97d7865ff357ffff9849c0e193bcb72b1f5fe7a4
suzuki.r <getty2017@gmail.com>
2020-04-26 21:51:18 +0900

    フォームのテキストに名前を入力したら、2ページ目でテキストの値を表示する

--- SecondKadaiApp/ViewController.swift
+++ SecondKadaiApp/ViewController.swift
@@ -9,11 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view.
-  }
+  
+  
+    @IBOutlet weak var inputText: UITextField!
+  
+    override func viewDidLoad() {
+      super.viewDidLoad()
+      // Do any additional setup after loading the view.
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // segueから遷移先のResultViewControllerを取得する
+        let resultViewController:ResultViewController = segue.destination as! ResultViewController
+        resultViewController.text = inputText.text!
+    }
+    
+    @IBAction func unwind(_ segue: UIStoryboardSegue) {
+    }
 
 
 }
